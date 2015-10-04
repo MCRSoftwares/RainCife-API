9eeb6561350cd3e206a9abbc7f658f37e84548e0
Victor Ferraz <vfsf@cin.ufpe.br>
2015-10-04 09:27:32 -0300

    Refatorando estrutura do projeto
    - Adicionado 'python-decouple' para separar a configuração do código
    - Adicionado Makefile para facilitar comandos
    - Adicionado example.settings.ini como guia para criação do settings.ini
    - Adicionadas novas linhas no .gitignore

---
+++ raincife_rest/raincife_rest/settings/installed_apps.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+LOCAL_APPS = (
+    'view_manager',
+    'apac',
+)
+
+THIRD_PARTY_APPS = (
+    'rest_framework',
+)
--- raincife_rest/raincife_rest/urls.py
+++ raincife_rest/raincife_rest/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='api')),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
     url(r'^apac/', include('apac.urls', namespace='apac')),
 ]
