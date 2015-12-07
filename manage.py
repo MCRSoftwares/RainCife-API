1c0f31d6d2428a00198539a4bf72c0a0c5f6fbc0
Victor Ferraz <vfsf@cin.ufpe.br>
2015-12-07 20:32:11 -0300

    Adicionando módulo de commandos

---
+++ manage.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from tornado.web import Application
+from tornado.ioloop import IOLoop
+from raincife.routers import routers
+from decouple import config
+from jsonado.core.utils import TableFinder
+from jsonado.core.utils import Commands
+import sys
+
+
+class RaincifeCommands(Commands):
+
+    def cmd_serve(self, *args):
+        app = Application(routers)
+        if len(args) > 0:
+            port = int(args[0])
+        else:
+            port = config('PORT', default=8888, cast=int)
+        app.listen(port)
+        IOLoop.current().start()
+
+
+RaincifeCommands(*sys.argv)
