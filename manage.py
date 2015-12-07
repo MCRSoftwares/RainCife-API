0e7b39b2542f94e74f43efbfe09b19e96584387b
Victor Ferraz <vfsf@cin.ufpe.br>
2015-12-07 20:59:37 -0300

    Add 'sync' command to create rethinkdb tables

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ from raincife.routers import routers
 from decouple import config
 from jsonado.core.utils import TableFinder
 from jsonado.core.utils import Commands
+import rethinkdb as r
 
 
 class RaincifeCommands(Commands):
@@ -17,7 +18,28 @@ class RaincifeCommands(Commands):
         else:
             port = config('PORT', default=8888, cast=int)
         app.listen(port)
+        r.set_loop_type('tornado')
         IOLoop.current().start()
+
+    def cmd_sync(self, *args):
+        tables = TableFinder().find('raincife/apps/')
+        c = r.connect(
+            port=config('DB_PORT', default=28015, cast=int),
+            host=config('DB_HOST', default='localhost')
+        )
+        for obj in tables:
+            r.db_list().contains(obj.db).do(
+                lambda db_exists: r.branch(
+                    db_exists,
+                    {'created': 0},
+                    r.db_create(obj.db))
+            ).run(c)
+            r.db(obj.db).table_list().contains(obj.table).do(
+                lambda table_exists: r.branch(
+                    table_exists,
+                    {'created': 0},
+                    r.db(obj.db).table_create(obj.table))
+            ).run(c)
 
 
 RaincifeCommands()
