49410e5e3cbb6afab860fe67d1ea3aa80512382c
Victor Ferraz <vfsf@cin.ufpe.br>
2015-12-12 16:32:19 -0300

    Adicionando decorador 'table.reql' em UserManager.fields

--- raincife/apps/users/db/managers.py
+++ raincife/apps/users/db/managers.py
@@ -14,5 +14,6 @@ class UserManager(tables.Manager):
     def get_reql(self):
         return UserReQL(db=self)
 
+    @tables.reql
     def fields(self, *args):
         return self.get_reql().fields(*args)
